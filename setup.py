2f850f8cd98997bbe636bd75fdbed3048784b3b4
NagaoKyota <kyota.n.2014@gmail.com>
2017-09-29 15:24:23 +0900

    adf

--- setup.py
+++ setup.py
@@ -20,3 +20,40 @@ setup(
     zip_safe=False,
     # could also include long_description, download_url, classifiers, etc.
 )
+
+
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+'''
+    >>> python test.py test.mp3
+'''
+
+import os, sys
+from acrcloud.recognizer import ACRCloudRecognizer
+from acrcloud.recognizer import ACRCloudRecognizeType
+
+if __name__ == '__main__':
+    config = {
+        'host':'identify-ap-southeast-1.acrcloud.com',
+        'access_key':'1a0cef2f6b6e1cb2aa1060d84260b83a',
+        'access_secret':'nAP54sQfL16WNYCbmo6HEbjqgfHVdVO8aLpekfmL',
+        'recognize_type': ACRCloudRecognizeType.ACR_OPT_REC_AUDIO, # you can replace it with [ACR_OPT_REC_AUDIO,ACR_OPT_REC_HUMMING,ACR_OPT_REC_BOTH], The     SDK decide which type fingerprint to create accordings to "recognize_type".
+        'debug':False,
+        'timeout':10 # seconds
+    }
+    
+    '''This module can recognize ACRCloud by most of audio/video file. 
+        Audio: mp3, wav, m4a, flac, aac, amr, ape, ogg ...
+        Video: mp4, mkv, wmv, flv, ts, avi ...'''
+    re = ACRCloudRecognizer(config)
+
+    #recognize by file path, and skip 0 seconds from from the beginning of sys.argv[1].
+    print(re.recognize_by_file(sys.argv[1], 0, 10))
+
+    print("duration_ms=" + str(ACRCloudRecognizer.get_duration_ms_by_file(sys.argv[1])))
+
+    buf = open(sys.argv[1], 'rb').read()
+    #recognize by file_audio_buffer that read from file path, and skip 0 seconds from from the beginning of sys.argv[1].
+    print(re.recognize_by_filebuffer(buf, 0, 10))
+
